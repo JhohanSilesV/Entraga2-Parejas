584f8620c70637359410f3d5405d8862aa03d964
Jhohan Eddy Siles Villarroel <CallMeSiles@users.noreply.github.com>
2020-12-05 14:10:14 +0100

    05.12.2020-14:07

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float playerForce = 1.0f;   //Fuerza de movimiento
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //      CONTROLES
+        GetComponent<Rigidbody>().AddForce((transform.right * playerForce * -Input.GetAxis("Horizontal")) + (transform.forward * playerForce * -Input.GetAxis("Vertical")));
+    }
+}
